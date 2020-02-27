241e281d617e979ab3015886cd02fb9393ee7e3c
bdiaz666 <47260214+bdiaz666@users.noreply.github.com>
2020-02-26 17:33:52 -0800

    bdiaz666 hw1 (#2)
    * bdiaz666 hw1
    
    * create the csv file
    
    * fixed the csv file
    
    Co-authored-by: Calvin Tam <51683307+ctam4@users.noreply.github.com>

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const axios = require('axios');
 app.use(compression());
 
 app.use('/', rootRoute);
+app.use('/bdiaz666',require('./routes/bdiaz666'))
 app.use('/jason1819', require('./routes/jason1819'));
 app.use('/ctam4', require('./routes/ctam4'));
 
