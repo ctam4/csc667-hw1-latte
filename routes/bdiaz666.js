f32a945cdb76a3353a6e5ea5b335bece7e85a84d
bdiaz666 <47260214+bdiaz666@users.noreply.github.com>
2020-02-26 17:33:52 -0800

    bdiaz666 hw1 (#2)
    * bdiaz666 hw1
    
    * create the csv file
    
    * fixed the csv file
    
    Co-authored-by: Calvin Tam <51683307+ctam4@users.noreply.github.com>

---
+++ routes/bdiaz666.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", (req, res) => res.send("Hello from bdiaz666!"));
+router.get("/weather", async (req, res) => {
+  const params = req.query;
+  let status, response;
+  let data;
+
+  if (Object.keys(params).length == 1 && params.hasOwnProperty("city")) {
+    let weather = await axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+          params.city +
+          ",USA&appid=32a932ca5d7b679be8de6648fa3d50c4&units=imperial"
+      )
+      .then(res => {
+        status = "OK";
+        data = res.data.main.temp;
+      })
+      .catch(e => {
+        status = "Error";
+        data = "API error " + e;
+      });
+    const returnObject = {
+      status: status,
+      date: new Date(),
+      params: params,
+      response: data
+    };
+    res.send(returnObject);
+  }
+});
+
+module.exports = router;
